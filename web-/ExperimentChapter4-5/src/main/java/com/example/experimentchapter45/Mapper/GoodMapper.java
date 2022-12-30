1aafe17d0ef6d2ca533bce30ae554e376c55ab27
C juicy <58644847+IloveCjuicy@users.noreply.github.com>
2022-12-30 08:31:36 +0800

    Add files via upload
    **12.30**
    另外两位组员实现了功能增补，并完成实验报告。

---
+++ web-/ExperimentChapter4-5/src/main/java/com/example/experimentchapter45/Mapper/GoodMapper.java
@@ -0,0 +1,24 @@
+package com.example.experimentchapter45.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.experimentchapter45.Model.Good;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface GoodMapper extends BaseMapper<Good>
+{
+    @Select("SELECT * FROM good")
+    List<Good> findAllGoods();
+    @Insert({"insert into good(goodname,price,stock,cover) value(#{goodname},#{price},#{stock},#{cover)"})
+    int insertGood(Good good);
+    @Delete({"delete from good where id=${id}"})
+    int deleteById(@Param("id")Integer id);
+    @Delete({"delete from good where id >= ${id1} and id <=${id2}"})
+    int BatchDelete(@Param("id1")Object id1,@Param("id2")Object id2);
+    @Update({"update good set price=${price},stock=${stock} where id=${id}"})
+    int updateById(@Param("price")Object price,@Param("stock")Object stock,@Param("id")Object id);
+}
